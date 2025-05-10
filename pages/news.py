474913b043a7d7e1eafaec606358230dadbcd2ee
bhaskarbidyanta <bhaskar.bid18@gmail.com>
2025-05-10 11:16:19 +0530

    Update news.py

--- pages/news.py
+++ pages/news.py
@@ -233,11 +233,11 @@ if st.button("Get answer!") and selected_options :
         st.session_state.chat_history.append((option, response))
         #st.write(f"**{option}:**", response)
 
-user_input = st.text_input("Ask a question about the news:")
+#user_input = st.text_input("Ask a question about the news:")
 
-if user_input:
-    response = st.session_state.qa_chain.run(user_input)
-    st.session_state.chat_history.append((user_input, response))
+#if user_input:
+#    response = st.session_state.qa_chain.run(user_input)
+#    st.session_state.chat_history.append((user_input, response))
 
 # Display chat history
 for question, answer in st.session_state.chat_history:
