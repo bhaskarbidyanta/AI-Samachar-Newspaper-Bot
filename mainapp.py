d584b7f3c32b10e1a2ebd53f5e01e5898fff3baa
bhaskarbidyanta <bhaskar.bid18@gmail.com>
2025-05-24 04:46:37 +0530

    some updates

--- mainapp.py
+++ mainapp.py
@@ -16,12 +16,8 @@ if "selected_page" not in st.session_state:
     st.session_state.selected_page = "Home"  # default page
 
 # Show navbar and update only if changed
-selected = show_navbar()
-if selected != st.session_state.selected_page:
-    st.session_state.selected_page = selected
-    st.rerun()
+selected_page = show_navbar()
 
-selected_page = st.session_state.selected_page
 
 if selected_page == "Home":
     st.title("Welcome to AI Samachar - Home Page")
